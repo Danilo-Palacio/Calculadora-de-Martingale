12cc933eb722b5b7ad2506aac0989790e1818128
Danilo Palacio <114814797+Danilo-Palacio@users.noreply.github.com>
2023-01-03 15:36:09 -0300

    Update scripts.js
    Feito a automação da busca das contas e calculo de cada martingale

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,136 +3,125 @@ var newAccount1 = document.querySelector('.newaccount1')
 var newAccount2 = document.querySelector('.newaccount2')
 var newAccount3 = document.querySelector('.newaccount3')
 var newAccount4 = document.querySelector('.newaccount4')  
+let conta;
 
 
 
-function cliqueMetade(){
+
+function cliqueMetade(){ // Botão de Metade
     inputQuantia.value = inputQuantia.value / 2 ;
 }
-
-function cliqueDobro(){
+function cliqueDobro(){ // Botão de Dobro
     inputQuantia.value = inputQuantia.value * 2 ;
 }
-
-
-    let num;
-
-    
-   
-function btnNovaConta(){
+function btnNovaConta(){ // Botão para fazer a conta e criar uma nova
+// faz o teste para ver qual está disponivel e envia para variavel num 
  
-    let btnClean = document.querySelector(`.btn${num}`)
-    let account = document.querySelector(`.account${num}`)
-    let headerCardTitle = document.querySelector(`.title${num}`)
-    let headerCardMultiple = document.querySelector(`.multiple${num}`)
-    let newAccount = document.querySelector(`.newaccount${num}`)
-
     if (newAccount1.style.display !== "none"){
-        
-        num = 1
-
-        account.style.display = "flex";
-        btnClean.style.display = "inline-block";
-        headerCardTitle.innerHTML = `Conta ${num}`;
-        headerCardMultiple.innerHTML = "1.5X";
-        newAccount.style.display = "none";
-
-        let aposta = document.querySelector('.card-account-description')
-        aposta.innerHTML = inputQuantia.value
-
-        num = 0
+        conta = 1
+        criarConta();
+        repGale()
+        conta = 0
 
     }else if (newAccount2.style.display !== "none"){
-
-        num = 2
-
-        account.style.display = "flex";
-        btnClean.style.display = "inline-block";
-        headerCardTitle.innerHTML = `Conta ${num}`;
-        headerCardMultiple.innerHTML = "1.5X";
-        newAccount.style.display = "none";
-
-        let aposta = document.querySelector('.card-account-description')
-        aposta.innerHTML = inputQuantia.value
-
-        num = 0
+        conta = 2
+        criarConta();
+        repGale()
+        conta = 0
         
     }else if (newAccount3.style.display !== "none"){
-        
-        num = 3
-
-        account.style.display = "flex";
-        btnClean.style.display = "inline-block";
-        headerCardTitle.innerHTML = `Conta ${num}`;
-        headerCardMultiple.innerHTML = "1.5X";
-        newAccount.style.display = "none";
-        
-        let aposta = document.querySelector('.card-account-description')
-        aposta.innerHTML = inputQuantia.value
-
-        num = 0
+        conta = 3
+        criarConta();
+        repGale()
+        conta = 0
 
     }else if (newAccount4.style.display !== "none"){
-        
-        num = 4
-
-        account.style.display = "flex";
-        btnClean.style.display = "inline-block";
-        headerCardTitle.innerHTML = `Conta ${num}`;
-        headerCardMultiple.innerHTML = "1.5X";
-        newAccount.style.display = "none";
-
-        let aposta = document.querySelector('.card-account-description')
-        aposta.innerHTML = inputQuantia.value
-
-        num = 0
+        conta = 4
+        criarConta();
+        repGale()
+        conta = 0
 
     }else {
         alert('limpe um dos campos para adicionar novas contas')
     }
-    
 }
+function criarConta(){//utiliza a variavel num para mostrar os campos
+    
+    let btnClean = document.querySelector(`.btn${conta}`)
+    let account = document.querySelector(`.account${conta}`)
+    let headerCardTitle = document.querySelector(`.title${conta}`)
+    let headerCardMultiple = document.querySelector(`.multiple${conta}`)
+    let newAccount = document.querySelector(`.newaccount${conta}`)
+    
+    account.style.display = "flex";
+    btnClean.style.display = "inline-block";
+    headerCardTitle.innerHTML = `Conta ${conta}`;
+    headerCardMultiple.innerHTML = "1.5X";
+    newAccount.style.display = "none";
+}
+function repGale(){ // Faz a repetição dos Gales com base na conta
+    
+    for (let i = 1; i <=3 ; i++) {
 
-function limparConta(num){
+        if (i == 1){
 
+            somaGale(1)
+
+        }else if (i == 2){
+
+            somaGale(2)
+
+        }else {
+
+            somaGale(3)
+
+        }
+    }
+}   
+function somaGale(i){ // Faz o calculo dos Martingales
+    let aposta = document.querySelector(`.aposta-${conta}-gale-${i}`)
+    let ganhou = document.querySelector(`.ganhou-${conta}-gale-${i}`)
+    let perdeu = document.querySelector(`.perdeu-${conta}-gale-${i}`)
+    let branco = document.querySelector(`.branco-${conta}-gale-${i}`)
+    
+    let contaAposta = inputQuantia.value * i; // Quanto tem que apostar
+        contaAposta = contaAposta.toFixed(1);
+    let contaGanhou = inputQuantia.value * i * 2;// Se ganhar
+        contaGanhou = contaGanhou.toFixed(1);
+    let valorBranco = inputQuantia.value * 14; // Se der branco
+        valorBranco = valorBranco.toFixed(1);
+    let contaPerdeu = inputQuantia.value * i; // Se perder
+        contaPerdeu = contaPerdeu.toFixed(1);
+    
+
+    aposta.innerHTML = `R$${contaAposta}`
+    ganhou.innerHTML = `R$${contaGanhou}`
+    branco.innerHTML = `R$${valorBranco}`
+    perdeu.innerHTML = `R$${contaPerdeu}`
+
+}
+function limparConta(num){ // botão de limpar a conta
+
+    function buscaLimparConta(){// função para limpar a conta
+        btnClean.style.display = 'none'
+        account.style.display = 'none'
+        headerCardTitle.innerHTML = " ";
+        headerCardMultiple.innerHTML = " ";
+        newAccount.style.display = "flex";
+    }
     let btnClean = document.querySelector(`.btn${num}`)
     let account = document.querySelector(`.account${num}`)
     let headerCardTitle = document.querySelector(`.title${num}`)
     let headerCardMultiple = document.querySelector(`.multiple${num}`)
     let newAccount = document.querySelector(`.newaccount${num}`)
 
-    if (num == 1 ){
-        
-        btnClean.style.display = 'none'
-        account.style.display = 'none'
-        headerCardTitle.innerHTML = " ";
-        headerCardMultiple.innerHTML = " ";
-        newAccount.style.display = "flex";
-
+    if (num == 1 ){ // Faz o teste e limpa a conta certa
+        buscaLimparConta()
     }else if (num == 2){
-
-        btnClean.style.display = 'none'
-        account.style.display = 'none'
-        headerCardTitle.innerHTML = " ";
-        headerCardMultiple.innerHTML = " ";
-        newAccount.style.display = "flex";
-
+        buscaLimparConta()
     }else if (num == 3){
-
-        btnClean.style.display = 'none'
-        account.style.display = 'none'
-        headerCardTitle.innerHTML = " ";
-        headerCardMultiple.innerHTML = " ";
-        newAccount.style.display = "flex";
-
+        buscaLimparConta()
     }else if (num == 4){
-
-        btnClean.style.display = 'none'
-        account.style.display = 'none'
-        headerCardTitle.innerHTML = " ";
-        headerCardMultiple.innerHTML = " ";
-        newAccount.style.display = "flex";
-        
+        buscaLimparConta()
     }
-
 }
